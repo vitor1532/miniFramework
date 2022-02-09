a9cb0a98369654239eea2f51a68324ac22d903e5
unknown <vitomartinsalmeida@gmail.com>
2022-02-09 17:11:52 -0300

    anotações da aula

--- anotações.php
+++ anotações.php
@@ -49,15 +49,37 @@ Criamos a pasta "controller" dentro do diretório MF, e dentro da pasta criada, 
 
 //AULA DEFININDO E REUTILIZANDO UM LAYOUT
 
-<!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
 <!-- Bootstrap CDN -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+A proposta é pegar códigos que são comuns a todas as paginas da aplicação e usa-lo em um só script para evitar ficar repetindo códigos
+
+Criamos um arquivo dentro do dir Views chamado 'layout.phtml' para que todas as views sejam renderizadas dentro do layout criado
+
+fazemos a inclusão dentro do layout com o comando <?= $this->content() ?>
+
+dentro da classe Action, criamos o método content() passando a lógica de renderização do método render() seja passada para o método criado (content())
+o metodo render, então será responsável por renderizar o layout, e o layout será responsável pela renderização.
+dentro de render, chamamos o método $this->content()
+para a chamada funcionar, usamos um require_once do layout que já está chamando o método $this->content()
+para resolver o problema do parametro $view em render ser vazio, dentro desse método, adicionamos um atributo à instancia do obj view com a linha '$this->view->page = $view;', criando o parametro 'page'
+
+então, no método contente, mudamos o parametro dinamico $view em require_once para $this->view->page
+
+DEFININDO O LAYOUT COMO DINAMICO:
+
+Em indexController.php, nas funções de render, chamamos mais um parametro, sendo ele o layout de sua preferencia
+
+Já na Action.php, passamos a recebero parametro do layout (render($view, $layout))
+modificando o require do mesmo, passando require_once "../App/Views".$layout.".phtml";
+
+implementamos por fim, um if(file_exists) verificando se o arquivo de layout existe para então, renderiza-lo
+usamos o else para renderizar a pagina sem layout, caso o mesmo não exista
+
 
 */
 ?>
