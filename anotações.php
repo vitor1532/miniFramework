1a326dee4d49441f87a2bb8e55234d76ce451d45
unknown <vitomartinsalmeida@gmail.com>
2022-02-09 15:44:29 -0300

    adicionando bootstrap

--- anotações.php
+++ anotações.php
@@ -41,7 +41,23 @@ O PONTO DE PARTIDA DOS REQUIRES É SEMPRE DE INDEX.PHP (no dir public)
 
 Os models podem ser usados pelos controladores para recuperar informações de banco de dados, retorná-las aos controladores, que por sua vez, passam essas informações para a view, o consumidor final.
 
+//AULA ABSTRAÇÃO DE CONTROLLERS
+A ideia é isolar os requisitos funcionais da aplicação dos requisitos não funcionais, ligando os requisitos não funcionais diretamente ao framework
+Deixando assim, os controladores principais apenas com requisitos funcionais da aplicação
 
+Criamos a pasta "controller" dentro do diretório MF, e dentro da pasta criada, inserimos um script com o nome de Action.php, que receberá um namespace (para uso futuro) e todos os parametros não-funcionais da aplicação, deixando assim os controllers principais com um código mais enxuto
+
+//AULA DEFININDO E REUTILIZANDO UM LAYOUT
+
+<!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<!-- Bootstrap CDN -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 */
 ?>
